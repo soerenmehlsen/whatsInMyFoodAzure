2d2d2af60dbf21170f263cbd9b92b57b1a3a7c42
Søren Mehlsen <78858933+soerenmehlsen@users.noreply.github.com>
2025-01-23 23:14:46 +0100

    performance: using Next image component to optimize images on articles

--- app/components/articleShowcase.tsx
+++ app/components/articleShowcase.tsx
@@ -132,9 +132,11 @@ export default function ArticleShowcase() {
 const ArticleCard = ({ article, formatDate }: { article: article, formatDate: (date: string) => string }) => (
     <article className="group relative text-left overflow-hidden rounded-lg shadow hover:shadow-lg transition-shadow duration-300 w-full max-w-sm md:max-w-md lg:max-w-lg mx-auto sm:h-96 h-72">
         <div className="aspect-w-16 aspect-h-9 w-full">
-            <img
-                alt={article.title}
+            <Image
                 src={article.Image}
+                alt="Article image"
+                width={1024}
+                height={768}
                 className="absolute inset-0 h-full w-full object-cover transition-transform duration-300 group-hover:scale-105"
             />
         </div>
@@ -142,9 +144,11 @@ const ArticleCard = ({ article, formatDate }: { article: article, formatDate: (d
         <div className="relative h-full w-full bg-gradient-to-t from-gray-900/80 via-gray-900/50 to-transparent p-4 md:p-6">
             <div className="flex flex-col h-full justify-end">
                 <div className="flex absolute top-4 left-4">
-                    <img
+                    <Image
                         src={article.logo}
                         alt="Logo"
+                        width={280}
+                        height={280}
                         className="w-8 h-8 rounded-full"
                     />
                 </div>
