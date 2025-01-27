2384d3dfe941545dc29ea2b525f44416d4fb98c7
Søren Mehlsen <78858933+soerenmehlsen@users.noreply.github.com>
2025-01-27 17:33:01 +0100

    Add: Fade animation to the components in the landing page

--- app/components/HowItWorks.tsx
+++ app/components/HowItWorks.tsx
@@ -1,54 +1,53 @@
+import { Fade } from "react-awesome-reveal"
+
 const HowItWorks = () => {
+
+    const steps = [
+        {
+            number: 1,
+            title: "Take a picture",
+            description: "Take a picture of your food's ingredient list.",
+        },
+        {
+            number: 2,
+            title: "Process the image",
+            description: "The image will now be scanned for ingredients with AI.",
+        },
+        {
+            number: 3,
+            title: "Get the results",
+            description: "You will get a detailed breakdown of each ingredient and how processed it is.",
+        },
+    ];
+    
     return (
         <div>
+            <Fade direction="up" delay={300} cascade damping={0.1} triggerOnce={true}>
         <div>
             <h1 className="sm:mt-36 mt-20 text-balance text-4xl font-bold text-zinc-800">
                 Here&apos;s how it works
             </h1>
         </div>
-
-    <div className="max-w-7xl mx-auto flex flex-wrap justify-center sm:mt-20 mt-10">
-        <div className="w-full sm:w-1/3 px-4">
-            <div className="flex flex-col items-center sm:mt-2 mt-10">
-                <h1 className="text-2xl font-bold text-white mb-4 bg-black rounded-full w-12 h-12 flex items-center justify-center">
-                    1
-                </h1>
-                <h2 className="text-2xl font-bold text-zinc-800 mb-4">
-                    Take a picture
-                </h2>
-                <p className="text-gray-500">
-                    Take a picture of your food&apos;s ingredient list.
-                </p>
-            </div>
+                <div className="max-w-7xl mx-auto flex flex-wrap justify-center sm:mt-20 mt-10">
+                    {steps.map((step, index) => (
+                        <div key={index} className="w-full sm:w-1/3 px-4">
+                            <div className="flex flex-col items-center sm:mt-2 mt-10">
+                                <h1 className="text-2xl font-bold text-white mb-4 bg-black rounded-full w-12 h-12 flex items-center justify-center">
+                                    {step.number}
+                                </h1>
+                                <h2 className="text-2xl font-bold text-zinc-800 mb-4">
+                                    {step.title}
+                                </h2>
+                                <p className="text-gray-500">
+                                    {step.description}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </Fade>
         </div>
-        <div className="w-full sm:w-1/3 px-4">
-            <div className="flex flex-col items-center sm:mt-2 mt-10">
-                <h1 className="text-2xl font-bold text-white mb-4 bg-black rounded-full w-12 h-12 flex items-center justify-center">
-                    2
-                </h1>
-                <h2 className="text-2xl font-bold text-zinc-800 mb-4">
-                    Process the image
-                </h2>
-                <p className="text-gray-500">
-                    The image will now be scanned for ingredients with AI.
-                </p>
-            </div>
-        </div>
-        <div className="w-full sm:w-1/3 px-4">
-            <div className="flex flex-col items-center sm:mt-2 mt-10">
-                <h1 className="text-2xl font-bold text-white mb-4 bg-black rounded-full w-12 h-12 flex items-center justify-center">
-                    3
-                </h1>
-                <h2 className="text-2xl font-bold text-zinc-800 mb-4">
-                    Get the results
-                </h2>
-                <p className="text-gray-500">
-                    You will get a detailed breakdown of each ingredient and how processed it is.
-                </p>
-            </div>
-        </div>
-    </div>
-</div>
+        
     );
 }
 
--- app/components/WhyItMatters.tsx
+++ app/components/WhyItMatters.tsx
@@ -1,7 +1,9 @@
 import ArticleShowcase from "./articleShowcase";
+import { Fade } from "react-awesome-reveal"
 
 const WhyItMatters = () => {
   return (
+      <Fade direction="up" delay={300} cascade damping={0.1} triggerOnce={true}>
       <div>
           <h1 className="sm:mt-36 mt-20 text-balance text-4xl font-bold text-zinc-800">
               Why it matters
@@ -11,6 +13,7 @@ const WhyItMatters = () => {
               <ArticleShowcase />
           </div>
       </div>
+        </Fade>
   );
 }
 
