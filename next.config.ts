89f800b509e7bc2a5f62b603ef78fa77aaea4bc7
Søren Mehlsen <soeren48@hotmail.com>
2025-02-05 13:02:05 +0000

    Add: update PostHog ingestion rewrites and support for trailing slash API requests

--- next.config.ts
+++ next.config.ts
@@ -9,11 +9,21 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
+        source: "/ingest/static/:path*",
+        destination: "https://eu-assets.i.posthog.com/static/:path*",
+      },
+      {
         source: "/ingest/:path*",
-        destination: "https://eu.posthog.com/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
+      },
+      {
+        source: "/ingest/decide",
+        destination: "https://eu.i.posthog.com/decide",
       },
     ];
   },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true, 
 };
 
 export default nextConfig;
