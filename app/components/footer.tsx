ce3092059438c1e49fdffdba590964d0c4a903e0
Søren Mehlsen <soeren48@hotmail.com>
2025-02-06 15:07:37 +0000

    Add: copyright text to the footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,13 +1,21 @@
 import { FaGithub } from "react-icons/fa";
 export function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="bg-white border-t border-gray-200 mt-auto">
       <div className="container mx-auto px-4 py-4">
         <div className="flex flex-col items-center justify-between md:flex-row">
-          <div className="text-center md:text-left mb-4 md:mb-0">
-            <p className="text-sm text-gray-500">Created by Søren Mehlsen.</p>
+          <div className="text-center md:text-left mb-4 md:mb-0 md:w-1/3">
+            <p className="text-sm text-gray-500">Created by Søren Mehlsen</p>
           </div>
-          <div className="flex space-x-4">
+
+          <div className="text-center mb-4 md:mb-0 md:w-1/3">
+            <p className="text-sm text-gray-500">
+              <span>&copy; 2024-{currentYear} All rights reserved.</span>
+            </p>
+          </div>
+
+          <div className="flex justify-end md:w-1/3">
             <a
               href="https://github.com/soerenmehlsen"
               target="_blank"
